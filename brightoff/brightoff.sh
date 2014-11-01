0b21d2314a57c845cdf38fd8a1eb52eb2a80e81b
azubkov <artem.zubkoff@gmail.com>
2014-11-01 20:45:22 +0200

    script to off/on laptop screen: win+f1 default

---
+++ brightoff/brightoff.sh
@@ -0,0 +1,40 @@
+##script to switch laptop screem 
+## Dell Vostro 3560
+CURRPATH=$(pwd)
+#CURRPATH=/home/tort/drivers/brightoff
+COUNTER_FILE=$CURRPATH/brightoff.counter.file
+echo COUNTER_FILE : $COUNTER_FILE
+
+INDEX=$(cat $COUNTER_FILE) 
+
+
+if [ -n "$INDEX" ]; then
+    INDEX=$(($INDEX+1))    
+else
+    INDEX=1    
+fi
+
+echo current index is : $INDEX
+echo $INDEX > $COUNTER_FILE
+
+IS_MAKE_OFF=true
+
+if [ $(($INDEX%2)) -eq 0 ]; then
+    echo "even"
+    IS_MAKE_OFF=true
+else
+    echo "odd"
+    IS_MAKE_OFF=false
+fi
+
+i8kctl fan
+
+if [ "$IS_MAKE_OFF" = true ] ; then
+    echo 'turning off the laptop screen'
+    xset dpms force off
+else
+    echo 'turning on the laptop screen'
+    xset dpms force on
+fi
+
+##eof here
